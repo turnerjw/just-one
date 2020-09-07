40bfbd5bd7fdc51a1c4a9f490fe81e93fec9a944
Justin Turner <turnerjw@rogers.com>
2020-09-07 15:35:16 -0400

    Server code

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,27 @@
 import { Server } from "boardgame.io/server";
-// import path from "path";
-// import serve from "koa-static";
+import path from "path";
+import serve from "koa-static";
 import { JustOne } from "./Game";
 
 const server = Server({ games: [JustOne] });
 
-server.run(8000);
+//server.run(8000);
 
-// const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
-// console.log(PORT);
-// console.log(__dirname);
+console.log(PORT);
+console.log(__dirname);
 
-// // Build path relative to the server.js file
-// const frontEndAppBuildPath = path.resolve(__dirname, "../build");
-// server.app.use(serve(frontEndAppBuildPath));
+// Build path relative to the server.js file
+const frontEndAppBuildPath = path.resolve(__dirname, "../build");
+server.app.use(serve(frontEndAppBuildPath));
 
-// server.run(PORT as number, () => {
-//     server.app.use(
-//         async (ctx, next) =>
-//             await serve(frontEndAppBuildPath)(
-//                 Object.assign(ctx, { path: "index.html" }),
-//                 next
-//             )
-//     );
-// });
+server.run(PORT as number, () => {
+    server.app.use(
+        async (ctx, next) =>
+            await serve(frontEndAppBuildPath)(
+                Object.assign(ctx, { path: "index.html" }),
+                next
+            )
+    );
+});
